a237e14e126b4928011f3f973c564bb01112df53
Frederic Morin <fred.morin@gmail.com>
2022-11-03 12:58:28 -0400

    cleaned fcr data

--- data/init.sql
+++ data/init.sql
@@ -7,11 +7,11 @@
         marque_vehicule varchar,
         modele_vehicule varchar,
         annee_vehicule integer,
-        temps_auto double precision,
+        temps_auto double precision, -- secondes.
         temps_transport_commun double precision,
         temps_velo double precision,
         temps_marche double precision,
-        distance_auto double precision,
+        distance_auto double precision, -- metres.
         distance_transport_commun double precision,
         distance_velo double precision,
         distance_marche double precision
